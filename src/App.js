73c7c5032942c949c1ad3a6fe2616d9ecac91420
Azure Kelly <azkbaby@gmail.com>
2021-02-20 18:37:49 -0500

    Add initial color controls and basic styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
-function App() {
+import styled from 'styled-components';
+import ColorPanel from './ColorPanel';
+
+const Container = styled.div`
+    height: 100%;
+    background-color: #f3f3f3;
+`;
+
+const SidePane = styled.div`
+    width: 30%;
+    height: 100%;
+    overflow-y: auto;
+    background-color: #fff;
+    padding: 1rem 2rem;
+`;
+
+const App = () => {
   return (
-    <div></div>
+    <Container>
+        <SidePane>
+            <ColorPanel />
+        </SidePane>
+    </Container>
   );
 }
 
