c5c51f28b13b792b6e3403a9be62aa3638b335bd
Azure Kelly <azkbaby@gmail.com>
2021-03-10 05:04:09 -0800

    Fix formatting of AdvancedSection tests

--- src/components/AdvancedSection.spec.js
+++ src/components/AdvancedSection.spec.js
@@ -8,24 +8,29 @@ describe('AdvancedSection component', () => {
         render(<AdvancedSection />);
         expect(screen.getByRole('button', {name: /advanced/i})).toBeInTheDocument();
     });
+
     it('renders children when default collapse state is set to false', () => {
         render(<AdvancedSection collapsed={false}><div data-testid='child'>Child element</div></AdvancedSection>);
         expect(screen.getByTestId('child')).toBeInTheDocument();
     });
+
     it('does not render children when default collapse state is set to true', () => {
         render(<AdvancedSection collapsed={true}><div data-testid='child'>Child element</div></AdvancedSection>);
         expect(screen.queryByTestId('child')).not.toBeInTheDocument();
     });
+
     it('is collapsed by default and does not render children', () => {
         render(<AdvancedSection><div data-testid='child'>Child element</div></AdvancedSection>);
         expect(screen.queryByTestId('child')).not.toBeInTheDocument();
     });
+
     it('shows children when clicked if collapsed', () => {
         render(<AdvancedSection collapsed={true}><div data-testid='child'>Child element</div></AdvancedSection>);
         expect(screen.queryByTestId('child')).not.toBeInTheDocument();
         userEvent.click(screen.getByRole('button', {name: /advanced/i}));
         expect(screen.getByTestId('child')).toBeInTheDocument();
     });
+    
     it('hides children when clicked if uncollapsed', () => {
         render(<AdvancedSection collapsed={false}><div data-testid='child'>Child element</div></AdvancedSection>);
         expect(screen.getByTestId('child')).toBeInTheDocument();
