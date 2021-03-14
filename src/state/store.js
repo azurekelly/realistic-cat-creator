2c0f0f79a1c1f82674d566b6dc37c63c844a7ee0
Azure Kelly <azkbaby@gmail.com>
2021-03-14 05:17:50 -0700

    Add initial state to store

--- src/state/store.js
+++ src/state/store.js
@@ -2,8 +2,26 @@ import {createStore} from 'redux';
 import {devToolsEnhancer} from 'redux-devtools-extension';
 import rootReducer from './rootReducer';
 
+const initialState = {
+    cat: {
+        baseColor: 'black',
+        redness: 8, // visible on red-based cats, torties, and non-red tabbies
+        dilution: 8, // only visible on dilutes
+        baseMarking: 'solid', // can be solid, tabby, tortie, or torbie
+        pattern: 'mackerel', // only visible on non-solids, can be many options
+        patternContrast: 8,
+        silver: 'standard',
+        whiteSpread: 0,
+        point: 'standard',
+        eyePigment: 8,
+        blueRefraction: 8,
+        eyeColorBase: 'standard' // can be standard, aqua, or blue
+    }
+};
+
 export const store = createStore(
     rootReducer,
+    initialState,
     devToolsEnhancer()
 );
 
