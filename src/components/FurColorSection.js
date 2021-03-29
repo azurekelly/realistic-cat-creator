4af84f4cc84922a8d6be9a98588c4c170e7926a8
Azure Kelly <azkbaby@gmail.com>
2021-03-29 15:52:15 -0700

    Add state tests for FurColorSection

--- src/components/FurColorSection.js
+++ src/components/FurColorSection.js
@@ -1,23 +1,27 @@
+import {useDispatch} from 'react-redux';
 import Section from './Section';
+import {updateCat} from '../state/catState';
 
-const buttons = [
-    {label: 'Black'}, 
-    {label: 'Chocolate'}, 
-    {label: 'Cinnamon'}, 
-    {label: 'Red'}, 
-    {label: 'Gray'}, 
-    {label: 'Lilac'}, 
-    {label: 'Fawn'}, 
-    {label: 'Cream'}
-]
-;
-const sliders = [
-    {label: 'Redness'},
-    {label: 'Dilution'}
-];
+const FurColorSection = () => {
+    const dispatch = useDispatch();
 
-const FurColorSection = () => (
-    <Section title={'Fur color'} buttons={buttons} sliders={sliders} />
-);
+    const buttons = [
+        {label: 'Black', onClick: () => dispatch(updateCat({baseColor: 'black'}))},
+        {label: 'Chocolate', onClick: () => dispatch(updateCat({baseColor: 'chocolate'}))},
+        {label: 'Cinnamon', onClick: () => dispatch(updateCat({baseColor: 'cinnamon'}))},
+        {label: 'Red', onClick: () => dispatch(updateCat({baseColor: 'red'}))},
+        {label: 'Gray', onClick: () => dispatch(updateCat({baseColor: 'gray'}))},
+        {label: 'Lilac', onClick: () => dispatch(updateCat({baseColor: 'lilac'}))},
+        {label: 'Fawn', onClick: () => dispatch(updateCat({baseColor: 'fawn'}))},
+        {label: 'Cream', onClick: () => dispatch(updateCat({baseColor: 'cream'}))}
+    ]
+    ;
+    const sliders = [
+        {label: 'Redness', onChange: ({target: {value}}) => dispatch(updateCat({redness: value}))},
+        {label: 'Dilution', onChange: ({target: {value}}) => dispatch(updateCat({dilution: value}))}
+    ];
+
+    return <Section title={'Fur color'} buttons={buttons} sliders={sliders} />;
+};
 
 export default FurColorSection;
--- src/components/__tests__/FurColorSection.spec.js
+++ src/components/__tests__/FurColorSection.spec.js
@@ -1,38 +1,72 @@
 import FurColorSection from '../FurColorSection';
-import {render, screen} from '@testing-library/react';
+import {render, screen, fireEvent} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom/extend-expect';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from '../../state/rootReducer';
+import {catSelector} from '../../state/catState';
 
-const buttons = ['Black', 'Chocolate', 'Cinnamon', 'Red', 'Gray', 'Lilac', 'Fawn', 'Cream'];
-const sliders = ['Redness', 'Dilution'];
+const buttons = [
+    ['Black', state => catSelector(state).baseColor, 'black'],
+    ['Chocolate', state => catSelector(state).baseColor, 'chocolate'],
+    ['Cinnamon', state => catSelector(state).baseColor, 'cinnamon'],
+    ['Red', state => catSelector(state).baseColor, 'red'],
+    ['Gray', state => catSelector(state).baseColor, 'gray'],
+    ['Lilac', state => catSelector(state).baseColor, 'lilac'],
+    ['Fawn', state => catSelector(state).baseColor, 'fawn'],
+    ['Cream', state => catSelector(state).baseColor, 'cream']
+];
+const sliders = [
+    ['Redness', state => catSelector(state).redness],
+    ['Dilution', state => catSelector(state).dilution]
+];
+
+const renderComponent = (initialState = {}) => {
+    const store = createStore(reducer, initialState);
+    const view = render(<Provider store={store}><FurColorSection /></Provider>);
+    return {store, view};
+};
 
 // TODO add in tests related to disabling sliders based on current state
-// TODO add tests for changing state on button or slider event
 describe('FurColorSection component', () => {
     it('has section heading', () => {
-        render(<FurColorSection />);
+        renderComponent();
         expect(screen.getByText('Fur color')).toBeInTheDocument();
     });
 
     it('contains correct buttons', () => {
-        render(<FurColorSection />);
-        buttons.forEach(button => { 
+        renderComponent();
+        buttons.forEach(button => {
             // getByRole was being too slow for looping, jest sometimes timed out
-            expect(screen.getByText(button)).toBeInTheDocument();
-        })
+            expect(screen.getByText(button[0])).toBeInTheDocument();
+        });
+    });
+
+    it.each(buttons)('updates state when %s button clicked', (button, targetState, expected) => {
+        const {store} = renderComponent({cat: {baseColor: ''}});
+        userEvent.click(screen.getByRole('button', {name: button}));
+        expect(targetState(store.getState())).toBe(expected);
     });
 
     it('has an advanced section', () => {
-        render(<FurColorSection />);
+        renderComponent();
         expect(screen.getByText('Advanced')).toBeInTheDocument();
-    })
+    });
 
     it('displays correct sliders when advanced section is expanded', () => {
-        render(<FurColorSection />);
+        renderComponent();
         userEvent.click(screen.getByRole('button', {name: 'Advanced'}));
-        sliders.forEach(slider => {
+        sliders.forEach(([slider]) => {
             // getByRole was being too slow for looping, jest sometimes timed out
             expect(screen.getByLabelText(slider)).toBeInTheDocument();
-        })
+        });
+    });
+
+    it.each(sliders)('updates state when %s slider is changed', (slider, targetState) => {
+        const {store} = renderComponent({cat: {redness: 8, dilution: 8}});
+        userEvent.click(screen.getByRole('button', {name: 'Advanced'}));
+        fireEvent.change(screen.getByRole('slider', {name: slider}), {target: {value: 1}});
+        expect(targetState(store.getState())).toBe('1');
     });
 });
