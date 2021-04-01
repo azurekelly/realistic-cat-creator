1107ea085663e7a26b414e46e6c002a8b196ca60
Azure Kelly <azkbaby@gmail.com>
2021-03-31 18:02:18 -0700

    Add new sections to main panel

--- src/components/ColorPanel.js
+++ src/components/ColorPanel.js
@@ -1,9 +1,19 @@
 import FurColorSection from './sections/FurColorSection';
+import MarkingTypeSection from './sections/MarkingTypeSection';
+import PatternSection from './sections/PatternSection';
+import SilverSection from './sections/SilverSection';
+import PointSection from './sections/PointSection';
+import WhiteSection from './sections/WhiteSection';
 import EyeColorSection from './sections/EyeColorSection';
 
 const ColorPanel = () => (
     <>
         <FurColorSection />
+        <MarkingTypeSection />
+        <PatternSection />
+        <SilverSection />
+        <PointSection />
+        <WhiteSection />
         <EyeColorSection />
     </>
 );
