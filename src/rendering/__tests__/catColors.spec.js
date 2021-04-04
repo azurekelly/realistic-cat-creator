3ee0fc07aa158d89a62993ae138a4580a9316dd5
Azure Kelly <azkbaby@gmail.com>
2021-04-03 21:29:44 -0700

    Add the bulk of the color calculating functions

--- src/rendering/__tests__/catColors.spec.js
+++ src/rendering/__tests__/catColors.spec.js
@@ -1,4 +1,13 @@
-import {getEumelaninAbsorption, getPheomelaninAbsorption, simulateMelanin} from '../catColors';
+import {
+    getEumelaninAbsorption,
+    getPheomelaninAbsorption,
+    simulateMelanin,
+    CONSTANTS,
+    getBlackSettings,
+    getRedSettings,
+    getAgoutiSettings
+} from '../catColors';
+import {mapRange} from '../../utils/utils';
 
 describe('getEumelaninAbsorption', () => {
     it('returns expected absorption value for a given concentration', () => {
@@ -22,31 +31,284 @@ describe('getPheomelaninAbsorption', () => {
 
 describe('simulateMelanin', () => {
     it('returns expected rgb color for undiluted eumelanin', () => {
-        const melaninRgb = simulateMelanin('eumelanin', 400, 0);
+        const melaninRgb = simulateMelanin('eumelanin', 400, 0, 0);
         const expectedRgb = [72.151, 43.213, 30.712];
         melaninRgb.forEach((x, i) => {
             expect(melaninRgb[i]).toBeCloseTo(expectedRgb[i]);
         });
     });
+
     it('returns expected rgb color for diluted eumelanin', () => {
-        const melaninRgb = simulateMelanin('eumelanin', 500, 0.4);
+        const melaninRgb = simulateMelanin('eumelanin', 500, 0.4, 0);
         const expectedRgb = [136.126, 123.782, 120.098];
         melaninRgb.forEach((x, i) => {
             expect(melaninRgb[i]).toBeCloseTo(expectedRgb[i]);
         });
     });
+
+    it('returns expected rgb color for silver eumelanin', () => {
+        const melaninRgb = simulateMelanin('eumelanin', 300, 0, 0.6);
+        const expectedRgb = [191.4425, 176.078, 166.239];
+        melaninRgb.forEach((x, i) => {
+            expect(melaninRgb[i]).toBeCloseTo(expectedRgb[i]);
+        });
+    });
+
+    it('returns expected rgb color for diluted silver eumelanin', () => {
+        const melaninRgb = simulateMelanin('eumelanin', 900, 0.3, 0.5);
+        const expectedRgb = [177.804, 176.364, 176.250];
+        melaninRgb.forEach((x, i) => {
+            expect(melaninRgb[i]).toBeCloseTo(expectedRgb[i]);
+        });
+    });
+
     it('returns expected rgb color for undiluted pheomelanin', () => {
-        const melaninRgb = simulateMelanin('pheomelanin', 250, 0);
+        const melaninRgb = simulateMelanin('pheomelanin', 250, 0, 0);
         const expectedRgb = [182.744, 114.882, 56.876];
         melaninRgb.forEach((x, i) => {
             expect(melaninRgb[i]).toBeCloseTo(expectedRgb[i]);
         });
     });
+
     it('returns expected rgb color for diluted pheomelanin', () => {
-        const melaninRgb = simulateMelanin('pheomelanin', 350, 0.6);
+        const melaninRgb = simulateMelanin('pheomelanin', 350, 0.6, 0);
         const expectedRgb = [214.773, 186.866, 169.370];
         melaninRgb.forEach((x, i) => {
             expect(melaninRgb[i]).toBeCloseTo(expectedRgb[i]);
         });
     });
-})
+
+    it('returns expected rgb color for silver pheomelanin', () => {
+        const melaninRgb = simulateMelanin('pheomelanin', 375, 0, 0.35);
+        const expectedRgb = [185.590, 140.582, 114.421];
+        melaninRgb.forEach((x, i) => {
+            expect(melaninRgb[i]).toBeCloseTo(expectedRgb[i]);
+        });
+    });
+
+    it('returns expected rgb color for diluted silver pheomelanin', () => {
+        const melaninRgb = simulateMelanin('pheomelanin', 290, 0.4, 0.2);
+        const expectedRgb = [214.545, 181.168, 156];
+        melaninRgb.forEach((x, i) => {
+            expect(melaninRgb[i]).toBeCloseTo(expectedRgb[i]);
+        });
+    });
+});
+
+describe('getBlackSettings', () => {
+    it('handles black cats', () => {
+        const settings = getBlackSettings({baseColor: 'black', dilute: false, point: 'standard', silver: false});
+        expect(settings).toHaveLength(4);
+        expect(settings[0]).toBe('eumelanin');
+        expect(settings[1]).toBe(CONSTANTS.blackIntensity);
+        expect(settings[2]).toBe(0);
+        expect(settings[3]).toBe(0);
+    });
+
+    it('handles chocolate cats', () => {
+        const settings = getBlackSettings({baseColor: 'chocolate', dilute: false, point: 'standard', silver: false});
+        expect(settings[0]).toBe('eumelanin');
+        expect(settings[1]).toBe(CONSTANTS.chocolateIntensity);
+        expect(settings[2]).toBe(0);
+        expect(settings[3]).toBe(0);
+    });
+
+    it('handles cinnamon cats', () => {
+        const settings = getBlackSettings({baseColor: 'cinnamon', dilute: false, point: 'standard', silver: false});
+        expect(settings[0]).toBe('eumelanin');
+        expect(settings[1]).toBe(CONSTANTS.cinnamonIntensity);
+        expect(settings[2]).toBe(0);
+        expect(settings[3]).toBe(0);
+    });
+
+    it('handles dilute cats', () => {
+        const settings = getBlackSettings({baseColor: 'black', dilute: true, dilution: 6, point: 'standard', silver: false});
+        expect(settings[0]).toBe('eumelanin');
+        expect(settings[1]).toBe(CONSTANTS.blackIntensity);
+        expect(settings[2]).toBeCloseTo(mapRange(6, 0, 16, CONSTANTS.dilutionMin, CONSTANTS.dilutionMax));
+        expect(settings[3]).toBe(0);
+    });
+
+    it('handles silver cats', () => {
+        const settings = getBlackSettings({baseColor: 'black', dilute: false, point: 'standard', silver: true});
+        expect(settings[0]).toBe('eumelanin');
+        expect(settings[1]).toBe(CONSTANTS.blackIntensity);
+        expect(settings[2]).toBe(0);
+        expect(settings[3]).toBe(0); // base black should not change with silver, only agouti or red should
+    });
+
+    it('handles colorpoint dark colors', () => {
+        const colorpoints = ['point', 'mink', 'sepia'];
+        colorpoints.forEach(colorpoint => {
+            const settings = getBlackSettings({baseColor: 'black', dilute: false, point: colorpoint, silver: false}, false);
+            expect(settings[0]).toBe('eumelanin');
+            expect(settings[1]).toBeCloseTo(CONSTANTS.blackIntensity * CONSTANTS.pointDarkFactor); // dark color doesn't change between types
+            expect(settings[2]).toBe(0);
+            expect(settings[3]).toBe(0);
+        });
+    });
+
+    it('handles point light colors', () => {
+        const settings = getBlackSettings({baseColor: 'chocolate', dilute: false, point: 'point', silver: false}, true);
+        expect(settings[0]).toBe('eumelanin');
+        expect(settings[1]).toBeCloseTo(CONSTANTS.chocolateIntensity * CONSTANTS.pointLightFactor);
+        expect(settings[2]).toBe(0);
+        expect(settings[3]).toBe(0);
+    });
+
+    it('handles mink light colors', () => {
+        const settings = getBlackSettings({baseColor: 'black', dilute: false, point: 'mink', silver: false}, true);
+        expect(settings[0]).toBe('eumelanin');
+        expect(settings[1]).toBeCloseTo(CONSTANTS.blackIntensity * CONSTANTS.minkLightFactor);
+        expect(settings[2]).toBe(0);
+        expect(settings[3]).toBe(0);
+    });
+
+    it('has lighter point factor for black than other colors', () => {
+        const settings = getBlackSettings({baseColor: 'black', dilute: false, point: 'point', silver: false}, true);
+        expect(settings[0]).toBe('eumelanin');
+        expect(settings[1]).toBeCloseTo(CONSTANTS.blackIntensity * CONSTANTS.pointBlackLightFactor);
+        expect(settings[2]).toBe(0);
+        expect(settings[3]).toBe(0);
+    });
+
+    it('handles sepia light colors', () => {
+        const settings = getBlackSettings({baseColor: 'black', dilute: false, point: 'sepia', silver: false}, true);
+        expect(settings[0]).toBe('eumelanin');
+        expect(settings[1]).toBeCloseTo(CONSTANTS.blackIntensity * CONSTANTS.sepiaLightFactor);
+        expect(settings[2]).toBe(0);
+        expect(settings[3]).toBe(0);
+    });
+});
+
+describe('getRedSettings', () => {
+    it('handles red cats with varying redness', () => {
+        const rednesses = [4, 8, 12];
+
+        rednesses.forEach(redness => {
+            const settings = getRedSettings({redness: redness, dilute: false, point: 'standard', silver: false});
+            expect(settings).toHaveLength(4);
+            expect(settings[0]).toBe('pheomelanin');
+            expect(settings[1]).toBeCloseTo(mapRange(redness, 0, 16, CONSTANTS.redMinIntensity, CONSTANTS.redMaxIntensity));
+            expect(settings[2]).toBe(0);
+            expect(settings[3]).toBe(0);
+        });
+    });
+
+    it('handles cream cats', () => {
+        const settings = getRedSettings({redness: 6, dilute: true, dilution: 10, point: 'standard', silver: false});
+        expect(settings[0]).toBe('pheomelanin');
+        expect(settings[1]).toBeCloseTo(mapRange(6, 0, 16, CONSTANTS.redMinIntensity, CONSTANTS.redMaxIntensity));
+        expect(settings[2]).toBeCloseTo(mapRange(10, 0, 16, CONSTANTS.dilutionMin, CONSTANTS.dilutionMax));
+        expect(settings[3]).toBe(0);
+    });
+
+    it('handles silver cats', () => {
+        const settings = getRedSettings({redness: 9, dilute: false, point: 'standard', silver: true});
+        expect(settings[0]).toBe('pheomelanin');
+        expect(settings[1]).toBeCloseTo(mapRange(9, 0, 16, CONSTANTS.redMinIntensity, CONSTANTS.redMaxIntensity));
+        expect(settings[2]).toBe(0);
+        expect(settings[3]).toBe(CONSTANTS.redSilverFactor);
+    });
+
+    it('handles colorpoint dark colors', () => {
+        const colorpoints = ['point', 'mink', 'sepia'];
+        colorpoints.forEach(colorpoint => {
+            const settings = getRedSettings({redness: 2, dilute: false, point: colorpoint, silver: false});
+            expect(settings[0]).toBe('pheomelanin');
+            expect(settings[1]).toBeCloseTo(mapRange(2, 0, 16, CONSTANTS.redMinIntensity, CONSTANTS.redMaxIntensity) * CONSTANTS.pointRedFactor); // dark color doesn't change between types
+            expect(settings[2]).toBe(0);
+            expect(settings[3]).toBe(0);
+        });
+    });
+
+    it('handles point light colors', () => {
+        const settings = getRedSettings({redness: 13, dilute: false, point: 'point', silver: false}, true);
+        expect(settings[0]).toBe('pheomelanin');
+        expect(settings[1]).toBeCloseTo(mapRange(13, 0, 16, CONSTANTS.redMinIntensity, CONSTANTS.redMaxIntensity) * CONSTANTS.pointLightFactor);
+        expect(settings[2]).toBe(0);
+        expect(settings[3]).toBe(0);
+    });
+
+    it('handles mink light colors', () => {
+        const settings = getRedSettings({redness: 7, dilute: false, point: 'mink', silver: false}, true);
+        expect(settings[0]).toBe('pheomelanin');
+        expect(settings[1]).toBeCloseTo(mapRange(7, 0, 16, CONSTANTS.redMinIntensity, CONSTANTS.redMaxIntensity) * CONSTANTS.minkLightFactor);
+        expect(settings[2]).toBe(0);
+        expect(settings[3]).toBe(0);
+    });
+
+    it('handles sepia light colors', () => {
+        const settings = getRedSettings({redness: 2, dilute: false, point: 'sepia', silver: false}, true);
+        expect(settings[0]).toBe('pheomelanin');
+        expect(settings[1]).toBeCloseTo(mapRange(2, 0, 16, CONSTANTS.redMinIntensity, CONSTANTS.redMaxIntensity) * CONSTANTS.sepiaLightFactor);
+        expect(settings[2]).toBe(0);
+        expect(settings[3]).toBe(0);
+    });
+});
+
+describe('getAgoutiSettings', () => {
+    it('handles cats with varying redness', () => {
+        const rednesses = [2, 5, 15];
+
+        rednesses.forEach(redness => {
+            const settings = getAgoutiSettings({redness: redness, dilute: false, point: 'standard', silver: false});
+            expect(settings).toHaveLength(4);
+            expect(settings[0]).toBe('pheomelanin');
+            expect(settings[1]).toBeCloseTo(mapRange(redness, 0, 16, CONSTANTS.redMinIntensity, CONSTANTS.redMaxIntensity) * CONSTANTS.agoutiFactor);
+            expect(settings[2]).toBe(0);
+            expect(settings[3]).toBe(0);
+        });
+    });
+
+    it('handles silver cats', () => {
+        const settings = getAgoutiSettings({redness: 12, dilute: false, point: 'standard', silver: true});
+        expect(settings[0]).toBe('pheomelanin');
+        expect(settings[1]).toBeCloseTo(mapRange(12, 0, 16, CONSTANTS.redMinIntensity, CONSTANTS.redMaxIntensity) * CONSTANTS.agoutiFactor);
+        expect(settings[2]).toBe(0);
+        expect(settings[3]).toBe(CONSTANTS.agoutiSilverFactor);
+    });
+
+    it('handles dilute cats', () => {
+        const settings = getAgoutiSettings({redness: 14, dilute: true, dilution: 7, point: 'standard', silver: false});
+        expect(settings[0]).toBe('pheomelanin');
+        expect(settings[1]).toBeCloseTo(mapRange(14, 0, 16, CONSTANTS.redMinIntensity, CONSTANTS.redMaxIntensity) * CONSTANTS.agoutiFactor);
+        expect(settings[2]).toBe(mapRange(7, 0, 16, CONSTANTS.dilutionMin, CONSTANTS.dilutionMax));
+        expect(settings[3]).toBe(0);
+    });
+
+    it('handles colorpoint dark colors', () => {
+        const colorpoints = ['point', 'mink', 'sepia'];
+        colorpoints.forEach(colorpoint => {
+            const settings = getAgoutiSettings({redness: 13, dilute: false, point: colorpoint, silver: false});
+            expect(settings[0]).toBe('pheomelanin');
+            expect(settings[1]).toBeCloseTo(mapRange(13, 0, 16, CONSTANTS.redMinIntensity, CONSTANTS.redMaxIntensity) * CONSTANTS.agoutiFactor * CONSTANTS.pointRedFactor); // dark color doesn't change between types
+            expect(settings[2]).toBe(0);
+            expect(settings[3]).toBe(0);
+        });
+    });
+
+    it('handles point light colors', () => {
+        const settings = getAgoutiSettings({redness: 11, dilute: false, point: 'point', silver: false}, true);
+        expect(settings[0]).toBe('pheomelanin');
+        expect(settings[1]).toBeCloseTo(mapRange(11, 0, 16, CONSTANTS.redMinIntensity, CONSTANTS.redMaxIntensity) * CONSTANTS.agoutiFactor * CONSTANTS.pointLightFactor);
+        expect(settings[2]).toBe(0);
+        expect(settings[3]).toBe(0);
+    });
+
+    it('handles mink light colors', () => {
+        const settings = getAgoutiSettings({redness: 9, dilute: false, point: 'mink', silver: false}, true);
+        expect(settings[0]).toBe('pheomelanin');
+        expect(settings[1]).toBeCloseTo(mapRange(9, 0, 16, CONSTANTS.redMinIntensity, CONSTANTS.redMaxIntensity) * CONSTANTS.agoutiFactor * CONSTANTS.minkLightFactor);
+        expect(settings[2]).toBe(0);
+        expect(settings[3]).toBe(0);
+    });
+
+    it('handles sepia light colors', () => {
+        const settings = getAgoutiSettings({redness: 4, dilute: false, point: 'sepia', silver: false}, true);
+        expect(settings[0]).toBe('pheomelanin');
+        expect(settings[1]).toBeCloseTo(mapRange(4, 0, 16, CONSTANTS.redMinIntensity, CONSTANTS.redMaxIntensity) * CONSTANTS.agoutiFactor * CONSTANTS.sepiaLightFactor);
+        expect(settings[2]).toBe(0);
+        expect(settings[3]).toBe(0);
+    });
+});
--- src/rendering/catColors.js
+++ src/rendering/catColors.js
@@ -1,4 +1,31 @@
-import {blendAlpha} from '../utils/color-utils';
+import {rgbToHex, blendAlpha} from '../utils/color-utils';
+import {mapRange} from '../utils/utils';
+
+// CONSTANTS
+export const CONSTANTS = {
+    blackIntensity: 900,
+    chocolateIntensity: 400,
+    cinnamonIntensity: 225,
+    redMinIntensity: 225,
+    redMaxIntensity: 400,
+    dilutionMin: 0.3,
+    dilutionMax: 0.65,
+    agoutiFactor: 0.15,
+    agoutiSilverFactor: 0.8,
+    redSilverFactor: 0.1,
+    agoutiContrastMin: 0.275,
+    agoutiContrastMax: 0.65,
+    // for currently unknown reasons, silver cats appear much lighter than if you just change out the agouti color
+    // contrast values have been altered to temporarily fix this until the problem is solved
+    silverContrastMin: 0.55,
+    silverContrastMax: 0.85,
+    pointLightFactor: 0.05, // similar enough for black and red to be the same factors
+    pointBlackLightFactor: 0.025,
+    minkLightFactor: 0.16,
+    sepiaLightFactor: 0.4,
+    pointDarkFactor: 0.9,
+    pointRedFactor: 0.6 // darker factor is more different so it needs it own
+};
 
 export const getEumelaninAbsorption = concentration => {
     // tweaked from the canonical absorption values to look nicer
@@ -12,7 +39,7 @@ export const getPheomelaninAbsorption = concentration => {
     return pheomelaninAbsorption.map(x => x * (concentration / 100));
 };
 
-export const simulateMelanin = (melaninType, intensity, dilution) => {
+export const simulateMelanin = (melaninType, intensity, dilution, silvering) => {
     const absorption = melaninType === 'pheomelanin' ? getPheomelaninAbsorption(intensity) : getEumelaninAbsorption(intensity);
     // "magic number" used to lighten colors closer to what we'd perceive as the "local color"
     const ambientOffest = 30;
@@ -20,6 +47,84 @@ export const simulateMelanin = (melaninType, intensity, dilution) => {
     // gives more accurate colors than a reflectance formula
     const transmittance = absorption.map(x => Math.exp(-x));
     const melaninRgb = transmittance.map(x => x * 255 + ambientOffest);
-    return blendAlpha(melaninRgb, [255, 255, 255], dilution); // dilutes color
+    const silverRgb = blendAlpha(melaninRgb, [255, 255, 255], silvering); // performs silvering dilution
+    return blendAlpha(silverRgb, [255, 255, 255], dilution); // dilutes color
 };
 
+export const getBlackSettings = (cat, pointLight = false) => {
+    const baseIntensity = CONSTANTS[cat.baseColor + 'Intensity'];
+    let pointFactor = 1;
+    if(cat.point !== 'standard') {
+        if(pointLight) {
+            if(cat.point === 'point' && cat.baseColor === 'black') {
+                pointFactor = CONSTANTS.pointBlackLightFactor;
+            }
+            else {
+                pointFactor = CONSTANTS[cat.point + 'LightFactor'];
+            }
+        }
+        else {
+            pointFactor = CONSTANTS.pointDarkFactor;
+        }
+    }
+    return [
+        'eumelanin',
+        baseIntensity * pointFactor,
+        cat.dilute ? mapRange(cat.dilution, 0, 16, CONSTANTS.dilutionMin, CONSTANTS.dilutionMax) : 0,
+        0
+    ];
+};
+
+export const getRedSettings = (cat, pointLight = false) => {
+    const baseIntensity = mapRange(cat.redness, 0, 16, CONSTANTS.redMinIntensity, CONSTANTS.redMaxIntensity);
+    const pointFactor = cat.point === 'standard' ? 1 : (
+        pointLight ? CONSTANTS[cat.point + 'LightFactor'] : CONSTANTS.pointRedFactor
+    );
+    return [
+        'pheomelanin',
+        baseIntensity * pointFactor,
+        cat.dilute ? mapRange(cat.dilution, 0, 16, CONSTANTS.dilutionMin, CONSTANTS.dilutionMax) : 0,
+        cat.silver ? CONSTANTS.redSilverFactor : 0
+    ];
+};
+
+export const getAgoutiSettings = (cat, pointLight = false) => {
+    const baseIntensity = mapRange(cat.redness, 0, 16, CONSTANTS.redMinIntensity, CONSTANTS.redMaxIntensity);
+    const pointFactor = cat.point === 'standard' ? 1 : (
+        pointLight ? CONSTANTS[cat.point + 'LightFactor'] : CONSTANTS.pointRedFactor
+    );
+    return [
+        'pheomelanin',
+        baseIntensity * pointFactor * CONSTANTS.agoutiFactor,
+        cat.dilute ? mapRange(cat.dilution, 0, 16, CONSTANTS.dilutionMin, CONSTANTS.dilutionMax) : 0,
+        cat.silver ? CONSTANTS.agoutiSilverFactor : 0
+    ];
+};
+
+const blendAgouti = (baseSettings, cat, pointLight = false) => {
+    const baseRgb = simulateMelanin(...baseSettings);
+    const agoutiRgb = simulateMelanin(...getAgoutiSettings(cat, pointLight));
+    if(cat.silver) {
+        return rgbToHex(blendAlpha(baseRgb, agoutiRgb, mapRange(cat.patternContrast, 0, 16, CONSTANTS.silverContrastMin, CONSTANTS.silverContrastMax)));
+    }
+    return rgbToHex(blendAlpha(baseRgb, agoutiRgb, mapRange(cat.patternContrast, 0, 16, CONSTANTS.agoutiContrastMin, CONSTANTS.agoutiContrastMax)));
+};
+
+// red methods need to be separate for ease of getting tortie colors
+export const getRedGroundColor = (cat, pointLight = false) => blendAgouti(getRedSettings(cat, pointLight), cat, pointLight);
+
+export const getRedTopColor = (cat, pointLight = false) => rgbToHex(simulateMelanin(...getRedSettings(cat, pointLight)));
+
+export const getBlackGroundColor = (cat, pointLight = false) => {
+    if(cat.tabby) {
+        return blendAgouti(getBlackSettings(cat, pointLight), cat, pointLight);
+    }
+    return rgbToHex(simulateMelanin(...getBlackSettings(cat, pointLight)));
+};
+
+export const getBlackTopColor = (cat, pointLight = false) => {
+    if(!cat.tabby && cat.silver) { // weird case for smoke only
+        return rgbToHex(simulateMelanin(...getAgoutiSettings(cat, pointLight)));
+    }
+    return rgbToHex(simulateMelanin(...getBlackSettings(cat, pointLight)));
+};
